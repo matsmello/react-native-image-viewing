b40ba60c664c5bf8ee89522529f5363b9dded5e6
Matheus Mello <matheusmello_22@hotmail.com>
2020-06-16 09:38:48 -0300

    Prod. Version

---
+++ dist/hooks/useImageDimensions.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) JOB TODAY S.A. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import { useEffect, useState } from "react";
+import { Image, Dimensions as RDimensions } from "react-native";
+import { createCache } from "../utils";
+const CACHE_SIZE = 50;
+const imageDimensionsCache = createCache(CACHE_SIZE);
+const { width: screenWidth, height: screenHeight } = RDimensions.get("screen");
+const useImageDimensions = (image) => {
+    const [dimensions, setDimensions] = useState(null);
+    const getImageDimensions = (image) => {
+        return new Promise((resolve) => {
+            const { width: originalW, height: originalH } = Image.resolveAssetSource(image);
+            if (typeof image == "number") {
+                const cacheKey = `${image}`;
+                let imageDimensions = imageDimensionsCache.get(cacheKey);
+                if (!imageDimensions) {
+                    imageDimensions = {
+                        width: originalW || screenWidth,
+                        height: originalH || screenHeight * 0.75,
+                    };
+                    imageDimensionsCache.set(cacheKey, imageDimensions);
+                }
+                resolve(imageDimensions);
+                return;
+            }
+            // @ts-ignore
+            if (image.uri) {
+                const source = image;
+                const cacheKey = source.uri;
+                const imageDimensions = imageDimensionsCache.get(cacheKey);
+                if (imageDimensions) {
+                    resolve({
+                        width: originalW || screenWidth,
+                        height: originalH || screenHeight * 0.75,
+                    });
+                }
+                else {
+                    // @ts-ignore
+                    Image.getSizeWithHeaders(source.uri, source.headers, (width, height) => {
+                        imageDimensionsCache.set(cacheKey, { width, height });
+                        resolve({
+                            width: width || originalW || screenWidth,
+                            height: height || originalH || screenHeight * 0.75,
+                        });
+                    }, () => {
+                        resolve({ width: 0, height: 0 });
+                    });
+                }
+            }
+            else {
+                resolve({ width: 0, height: 0 });
+            }
+        });
+    };
+    let isImageUnmounted = false;
+    useEffect(() => {
+        getImageDimensions(image).then((dimensions) => {
+            if (!isImageUnmounted) {
+                let newDimensions = dimensions.height == 0 || dimensions.width == 0
+                    ? { height: screenHeight * 0.75, width: screenWidth }
+                    : dimensions;
+                setDimensions(newDimensions);
+            }
+        });
+        return () => {
+            isImageUnmounted = true;
+        };
+    }, [image]);
+    return dimensions;
+};
+export default useImageDimensions;
